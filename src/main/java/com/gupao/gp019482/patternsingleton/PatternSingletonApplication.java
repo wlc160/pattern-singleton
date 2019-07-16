cbabc403a01c88199b182a7aeec2189e726b45b2
lcwen <1602251271@qq.com>
2019-07-16 21:48:39 +0800

    Initial commit

---
+++ src/main/java/com/gupao/gp019482/patternsingleton/PatternSingletonApplication.java
@@ -0,0 +1,13 @@
+package com.gupao.gp019482.patternsingleton;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PatternSingletonApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PatternSingletonApplication.class, args);
+    }
+
+}
